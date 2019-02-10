81d1e817ef88866b2f3a289f69e7d17ef3f80c35
Riley Williams <williams.riley@me.com>
2019-02-10 16:06:16 -0500

    Initial Commit

---
+++ Spot Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spot Me
+//
+//  Created by Riley Williams on 2/10/19.
+//  Copyright © 2019 Riley Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
